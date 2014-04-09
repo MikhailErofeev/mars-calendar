e6655ece6c7ab30b44035eb7f3f8b732767c0cb9
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-09 15:43:04 +0400

    oops, fix startup

--- src/main/java/com/github/mikhailerofeev/mars/calendar/server/Application.java
+++ src/main/java/com/github/mikhailerofeev/mars/calendar/server/Application.java
@@ -13,9 +13,10 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
   public static void main(String[] args) {
-    final SpringApplication springApplication = new SpringApplication(Application.class, args);
-    springApplication.setShowBanner(false);
-    springApplication.run(args);
+//    final SpringApplication springApplication = new SpringApplication(Application.class, args);
+//    springApplication.setShowBanner(false);
+//    springApplication.run(args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
