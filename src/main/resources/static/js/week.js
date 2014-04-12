3ae89f158295fed8485e62a162f78403d47fc311
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-12 15:02:11 +0400

    week view

---
+++ src/main/resources/static/js/week.js
@@ -0,0 +1,30 @@
+/**
+ * @author m-erofeev
+ * @since 12.04.14
+ */
+var days = ["Solis", "Lunae", "Martis", "Mercurii", "Jovis", "Veneris", "Saturni"]
+function generateDaysAndScale() {
+    var scale = $("<div class='scale'></div>");
+    scale.append("<div class='scale-row'>day</div>")
+    for (var i = 0; i < 25; i++) {
+        var hour = i != 0 ? i + ":00" : "";
+        var hourEl = $("<div class='scale-row'>" + hour + "</div>");
+        scale.append(hourEl)
+    }
+    var $days = $("#days");
+    $days.append(scale)
+    for (var dayNumber = 0; dayNumber < 7; dayNumber++) {
+        var day = $("<div class='day'></div>");
+        day.append("<div class='day-top'>" + days[dayNumber] + "</div>")
+        for (var i = 0; i < 24; i++) {
+            var hour = $("<div class='hour'></div>");
+            day.append(hour)
+        }
+        var hour = $("<div class='hour' style='height: 20px'></div>");
+        day.append(hour)
+        $days.append(day)
+    }
+}
+$(document).ready(function () {
+    generateDaysAndScale();
+});
