329dad5ac81addfbe5482d3cdd3bc6810eb78ee5
Генович Владимир <genovich.v.v@hotmail.com>
2014-04-12 22:51:41 +0400

    header + dialog

--- src/main/resources/static/js/week.js
+++ src/main/resources/static/js/week.js
@@ -11,11 +11,26 @@ function generateDaysAndScale() {
     }
     $("#days-table").find("thead").append(header);
     for (var hour = 0; hour < 25; hour++) {
-        var row;
-        if (hour == 24){
-            row = $("<tr class='tr-hour' style='height: 20px;'></tr>");
-        }else{
-            row = $("<tr class='tr-hour'></tr>");
+        for (var quarter = 0; quarter < 4; quarter++) {
+            var row;
+            if (hour == 24 && quarter == 3)
+                row = $("<tr class='hour' style='height: 5px;'></tr>");
+            else
+                row = $("<tr class='hour'></tr>");
+
+            var hourTxt = "";
+            if (quarter == 0)
+                hourTxt = hour + ":00";
+
+            row.append("<td class='scale-row'>" + hourTxt + "</td>")
+            for (day = 0; day < 7; day++) {
+                row.append(
+                        $("<td class='hour'></td>")
+                            .attr("hour", hour)
+                            .attr("day", day)
+                            .attr("quarter", quarter));
+            }
+            $("#days-table").find("tbody").append(row);
         }
         
         var hourTxt = hour + ":00";
