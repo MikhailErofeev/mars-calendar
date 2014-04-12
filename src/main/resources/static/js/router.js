301a582cbdc7450ceb40edffe911bf04e2455fa2
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-12 23:30:41 +0400

    routing with planets

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -5,33 +5,45 @@
 
 
 var Controller = Backbone.Router.extend({
-    routes: {
-//        "": "start", // Пустой hash-тэг
-//        "!/": "start", // Начальная страница
-//        "!/week1": "success", // Блок удачи
-        "!/week/:year-:mm-:dd": "week",
-        "!/month/:year-:mm": "month",
-        "!/year/:year": "year",
-        "!/error": "error" // Блок ошибки
+    routes: {"!/": "error" // Начальная страница
+    },
+    week: function (origin, alternative, year, month, day) {
+        console.log(origin, alternative, year, month, day);
     },
 
-    week: function (year, mm, dd) {
-        console.log(year, mm, dd);
-    },
-
-    month: function (year, mm) {
+    month: function (origin, year, mm) {
         console.log(year, mm);
     },
 
-    year: function (year) {
+    year: function (origin, year) {
         console.log(year);
     },
 
     error: function () {
         console.log("error")
+    },
+
+    initialize: function () {
+//        "": "start", // Пустой hash-тэг
+//        "!/": "start", // Начальная страница
+//        "!/week1": "success", // Блок удачи
+        var router = this,
+            routes = [
+                [ /^!\/week\/([a-z]+)\/([a-z]+)\/(\d+)\/(\d+)\/(\d+)$/, "week", this.week],
+                    [ /^!\/month\/([a-z]+)\/(\d+)\/(\d+)$/, "month", this.month],
+                    [ /^!\/year\/([a-z]+)\/(\d+)$/, "year", this.year]
+//        "!/month/:year-:mm": "month",
+//        "!/year/:year": "year",
+//        "!/error": "error" ]// Блок ошибки
+            ];
+        _.each(routes, function (route) {
+            router.route.apply(router, route);
+            // ^ Say it ten times fast
+        });
     }
 });
 
 var controller = new Controller(); // Создаём контроллер
+//controller.initialize()
 
 Backbone.history.start();  // Запускаем HTML5 History push    
