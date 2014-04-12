d7ce71c631f559a038cd8685c6401ee177faa1ac
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-12 10:11:43 +0400

    auth with twi

--- src/main/java/com/github/mikhailerofeev/mars/calendar/rest/controllers/GreetingsController.java
+++ src/main/java/com/github/mikhailerofeev/mars/calendar/rest/controllers/GreetingsController.java
@@ -4,10 +4,10 @@ import com.github.mikhailerofeev.mars.calendar.model.services.UserService;
 import com.github.mikhailerofeev.mars.calendar.rest.dto.Greeting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
@@ -16,7 +16,7 @@ import java.security.Principal;
  * @since 09.04.14
  */
 
-@Controller
+@RestController
 @Deprecated //for testing purposes
 public class GreetingsController {
 
