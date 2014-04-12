6af58dcab8b5a8b17a4d7075e5b3a4b35ce41007
Maxim <cuerta.espada@gmail.com>
2014-04-12 17:32:15 +0400

    Added test for PlanetDateTime

--- src/test/java/com/github/mikhailerofeev/mars/calendar/model/values/PlanetDateTimeTest.java
+++ src/test/java/com/github/mikhailerofeev/mars/calendar/model/values/PlanetDateTimeTest.java
@@ -1,33 +1,42 @@
 package com.github.mikhailerofeev.mars.calendar.model.values;
 
+import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
 
 /**
  * Created by Максим on 12.04.2014.
  */
 public class PlanetDateTimeTest {
-    @Test
-    public void testGetYear() throws Exception {
 
+    private DateTime timeNow;
+    private DateTime epochTime;
+    private DateTime zeroTime;
+
+    @Before
+    public void Setup()
+    {
+        timeNow     = DateTime.now();
+        epochTime   = new DateTime(1965, 7, 15, 1, 0);
+        zeroTime    = new DateTime(1, 1, 1, 0, 0);
     }
 
     @Test
-    public void testGetMonth() throws Exception {
+    public void testZeroEpochZeroTime() throws Exception{
+        PlanetDateTime marsZeroTime     = new PlanetDateTime(zeroTime);
+        PlanetDateTime marsZeroEpoch    = new PlanetDateTime(zeroTime, epochTime);
+
 
     }
 
-    @Test
-    public void testGetDay() throws Exception {
-
+    private void testGetTime(DateTime expected, DateTime asq){
+        assert(expected == asq);
     }
 
-    @Test
-    public void testGetHour() throws Exception {
-
+    private void testGetVal(int expected, int asq){
+        assert(expected == asq);
     }
 
-    @Test
-    public void testGetMinute() throws Exception {
-
-    }
 }
