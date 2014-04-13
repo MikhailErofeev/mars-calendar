61c43e769fe814fc9ab484f7761cf5140dcf9117
Maxim <cuerta.espada@gmail.com>
2014-04-13 13:36:05 +0400

    EpochFactory updated

--- src/main/java/com/github/mikhailerofeev/mars/calendar/model/values/time/EpochFactory.java
+++ src/main/java/com/github/mikhailerofeev/mars/calendar/model/values/time/EpochFactory.java
@@ -7,10 +7,6 @@ import org.joda.time.DateTime;
  */
 public class EpochFactory {
     static public DateTime getDefaultEpoch(){
-        return getMariner4FlyBy();
-    }
-
-    static public DateTime getMariner4FlyBy(){
-        return new DateTime(1965, 7, 15, 1, 0);
+        return new DateTime(1965, 7, 15, 1, 0);//mariner4flyby epoch
     }
 }
