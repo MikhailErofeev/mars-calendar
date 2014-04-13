4fc686ba4ec2066371691da5bff0ac2e36b91754
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-13 11:42:02 +0400

    full mvc in backbone

--- src/main/resources/static/js/router.js
+++ src/main/resources/static/js/router.js
@@ -4,29 +4,34 @@
  */
 
 var Controller = Backbone.Router.extend({
-    routes: {"!/": "error" // Начальная страница
-    },
+    routes: {},
+
     week: function (origin, alternative, year, month, day) {
-        AppState.originPlanet = origin;
-        AppState.alternativePlanet = alternative;
-        AppState.year = year;
-        AppState.month = month;
-        AppState.day = day;
-        var weekTable = generateWeekTable(24, 0, ["Su", "Mn", "Tu", "We", "Th", "Fr", "Sa"]);
-        $("#main").html(weekTable)
+        appState.set({
+            originPlanet: origin,
+            alternativePlanet: alternative,
+            year: year,
+            month: month,
+            day: day,
+            viewType: "week"
+        });
     },
 
     month: function (origin, year, month) {
-        AppState.originPlanet = origin;
-        AppState.year = year;
-        AppState.month = month;
-        var monthTable = generateMonthWithNewWeekStart(29);
-        $("#main").html(monthTable)
+        appState.set({
+            originPlanet: origin,
+            year: year,
+            month: month,
+            viewType: "month"
+        });
     },
 
     year: function (origin, year) {
-        AppState.originPlanet = origin;
-        AppState.year = year;
+        appState.set({
+            originPlanet: origin,
+            year: year,
+            viewType: "year"
+        });
     },
 
     error: function () {
@@ -39,9 +44,9 @@ var Controller = Backbone.Router.extend({
 //        "!/week1": "success", 
         var router = this,
             routes = [
-                [ /^!\/week\/([a-z]+)\/([a-z]+)\/(\d+)\/(\d+)\/(\d+)$/, "week", this.week],
-                [ /^!\/month\/([a-z]+)\/(\d+)\/(\d+)$/, "month", this.month],
-                [ /^!\/year\/([a-z]+)\/(\d+)$/, "year", this.year]
+                [ /^!\/week\/([a-z]+)\/([a-z]+)\/(\d+)\/(\d+)\/(\d+)\/?$/, "week", this.week],
+                [ /^!\/month\/([a-z]+)\/(\d+)\/(\d+)\/?$/, "month", this.month],
+                [ /^!\/year\/([a-z]+)\/(\d+)\/?$/, "year", this.year]
 //        "!/month/:year-:mm": "month",
 //        "!/year/:year": "year",
 //        "!/error": "error" ]
@@ -55,7 +60,5 @@ var Controller = Backbone.Router.extend({
 
 $(document).ready(function () {
     var controller = new Controller();
-//controller.initialize()
-
     Backbone.history.start();
 });
