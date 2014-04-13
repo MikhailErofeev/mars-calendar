1ef5f928d73f85b8c102f4ca926321991efc2f21
m-erofeev <m-erofeev@yandex-team.ru>
2014-04-13 07:00:39 +0400

    refactoring, planets unification

--- src/main/resources/static/js/week.js
+++ src/main/resources/static/js/week.js
@@ -3,38 +3,36 @@
  * @since 12.04.14
  */
 var days = ["Solis", "Lunae", "Martis", "Mercurii", "Jovis", "Veneris", "Saturni"]
-function generateDaysAndScale() {
+function generateDaysAndScale(planetHours, planetMinutes) {
     var header = $("<tr></tr>");
     header.append("<th class='scale-header'></th>")
     for (day = 0; day < 7; day++) {
         header.append("<th class='day-header'>" + days[day] + "</th>");
     }
     $("#days-table").find("thead").append(header);
-    for (var hour = 0; hour < 25; hour++) {
-        for (var quarter = 0; quarter < 4; quarter++) {
-            var row;
-            if (hour == 24 && quarter == 3)
-                row = $("<tr class='tr-hour' style='height: 5px;'></tr>");
-            else
-                row = $("<tr class='tr-hour'></tr>");
-
-            if (quarter == 0) {
-                hourTxt = hour + ":00";
-                row.append("<td rowspan='4' class='scale-row'>" + hourTxt + "</td>")
-            } 
-
-            for (day = 0; day < 7; day++) {
-                var $td = $("<td class='quarter'></td>");
-                if (quarter == 3) {
-                    $td.addClass("hour");
-                }
-                row.append(
-                    $td.attr("hour", hour).attr("day", day).attr("quarter", quarter));
-            }
-            $("#days-table").find("tbody").append(row);
+    ;
+    var planetQuarters = Math.ceil((planetHours * 60 + planetMinutes) / 15.0);
+    console.log(planetQuarters)
+    for (var quarter = 0; quarter < planetQuarters; quarter++) {
+        hour = quarter / 4;
+        var row = $("<tr class='tr-hour'></tr>");
+        if (quarter % 4 == 0) {
+            hourTxt = hour + ":00";
+            row.append("<td rowspan='4' class='scale-row'>" + hourTxt + "</td>")
         }
+
+        for (day = 0; day < 7; day++) {
+            var $td = $("<td class='quarter'></td>");
+            if (quarter % 4 == 3) {
+                $td.addClass("hour");
+            }
+            row.append(
+                $td.attr("hour", hour).attr("day", day).attr("quarter", quarter % 4));
+        }
+        $("#days-table").find("tbody").append(row);
     }
 }
 $(document).ready(function () {
-    generateDaysAndScale();
+//    generateDaysAndScale(24, 0);
+    generateDaysAndScale(24, 37);
 });
